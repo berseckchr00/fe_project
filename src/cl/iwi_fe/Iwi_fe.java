8040ce6563da0fb6867f1cf164f04de0350c34ab
Diego Gonzalez <diego.gonzalez@grupogtd.com>
2018-12-31 12:08:26 -0300

    init commit FE Project

---
+++ src/cl/iwi_fe/Iwi_fe.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cl.iwi_fe;
+
+import cl.nic.dte.util.XMLUtil;
+import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.xmlbeans.XmlException;
+import org.apache.xmlbeans.XmlObject;
+
+/**
+ *
+ * @author dgonzalezj
+ */
+public class Iwi_fe {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Iwi_fe iwi_fe = new Iwi_fe();
+        iwi_fe.dte();
+    }
+    
+    public void dte(){
+        try {
+            String xml_name = "test.xml";
+            XMLUtil xMLUtil = new XMLUtil ();
+            XmlObject result = XmlObject.Factory.parse(new File(xml_name));
+            byte[] fxml = xMLUtil.getCleanedII(result);
+            String str = new String(fxml, "ISO-8859-1");
+            new dte().add(str);
+        } catch (XmlException ex) {
+            Logger.getLogger(Iwi_fe.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Iwi_fe.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    
+    
+}
---
+++ src/cl/iwi_fe/dte.java
@@ -0,0 +1,262 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cl.iwi_fe;
+
+import cl.nic.dte.TimbreException;
+import cl.nic.dte.util.Utilities;
+import cl.nic.dte.util.XMLUtil;
+import cl.sii.siiDte.AUTORIZACIONDocument;
+import cl.sii.siiDte.AutorizacionType;
+import cl.sii.siiDte.DTEDefType;
+import cl.sii.siiDte.DTEDocument;
+import cl.sii.siiDte.EnvioDTEDocument;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.KeyException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.SignatureException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.security.spec.InvalidKeySpecException;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.crypto.NoSuchPaddingException;
+import javax.xml.crypto.MarshalException;
+import javax.xml.crypto.dsig.XMLSignatureException;
+import javax.xml.namespace.QName;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.apache.xmlbeans.XmlCursor;
+import org.apache.xmlbeans.XmlException;
+import org.apache.xmlbeans.XmlObject;
+import org.apache.xmlbeans.XmlOptions;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+/**
+ *
+ * @author dgonzalezj
+ */
+public class dte {
+    
+    public static final String BASIC_FILES_COMPANIES_PATH = "files/companies/iwi";
+    public static final String BASIC_FILES_USERS_PATH = "files/users/iwi";
+    
+    /**
+     * create XML signed
+     * @param xml 
+     */
+    public void add(String xml){
+        if(xml!=null){
+
+            try {
+                String xmlInput = xml.replaceAll("> <", "><");
+                
+                DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+                DocumentBuilder db = dbf.newDocumentBuilder();
+                
+                InputSource is = new InputSource();
+                is.setCharacterStream(new StringReader(xmlInput));
+                Document docDte = db.parse(is);
+                
+                HashMap<String, String> namespaces = new HashMap<String, String>();
+                namespaces.put("", "http://www.sii.cl/SiiDte");
+                XmlOptions opts = new XmlOptions();
+                opts.setLoadSubstituteNamespaces(namespaces);
+                opts.setCharacterEncoding("ISO-8859-1");
+                
+                XMLUtil xMLUtil = new XMLUtil ();
+                XmlObject result = XmlObject.Factory.parse(xml);
+                byte[] fxml = xMLUtil.getCleanedII(result);
+                String str = new String(fxml, "ISO-8859-1");
+                
+                DTEDocument doc = DTEDocument.Factory.parse(str,opts);
+                String rutEmisor = doc.getDTE().getDocumento().getEncabezado().getEmisor().getRUTEmisor();
+                
+                xmlInput = "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>"+doc.toString();
+                xmlInput = new UtilsDte().convertToUTF8(xmlInput);
+                is.setCharacterStream(new StringReader(xmlInput));
+                docDte = db.parse(is);
+                NodeList document = docDte.getElementsByTagName("DTE");
+                
+                System.out.println(xmlInput);
+                
+                this.generate_dte(xmlInput);
+                
+            } catch (XmlException ex) {
+                Logger.getLogger(Iwi_fe.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (UnsupportedEncodingException ex) {
+                Logger.getLogger(Iwi_fe.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (SAXException ex) {
+                Logger.getLogger(Iwi_fe.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                Logger.getLogger(Iwi_fe.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (ParserConfigurationException ex) {
+                Logger.getLogger(Iwi_fe.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
+    public boolean generate_dte(String xml){
+        try {
+            String idS ="SDOC";
+            String recepS ="60803000-K";
+            
+            HashMap<String, String> namespaces = new HashMap<String, String>();
+            namespaces.put("", "http://www.sii.cl/SiiDte");
+            XmlOptions opts = new XmlOptions();
+            opts.setLoadSubstituteNamespaces(namespaces);
+            
+            
+            DTEDocument doc = DTEDocument.Factory.parse(xml);
+            
+            
+            String rut = doc.getDTE().getDocumento().getEncabezado().getEmisor().getRUTEmisor();
+            String dteType = doc.getDTE().getDocumento().getEncabezado().getIdDoc().getTipoDTE().toString();
+            
+            String cafS = BASIC_FILES_COMPANIES_PATH+"/caf/"+dteType+"/33.xml";
+            
+            AutorizacionType caf = AUTORIZACIONDocument.Factory.parse(
+                    new File(cafS), opts).getAUTORIZACION();
+            
+            String certS = BASIC_FILES_USERS_PATH+"/cert/iwi.pfx";
+            
+            String planenvio = BASIC_FILES_COMPANIES_PATH+"/plantilla/"+dteType+".xml";
+            String plantillaEnvio = new Scanner(new File(planenvio), "ISO-8859-1").useDelimiter("\\A").next();
+            
+            String passS = "Charlie12";
+            // leo certificado y llave privada del archivo pkcs12
+            KeyStore ks = KeyStore.getInstance("PKCS12");
+            ks.load(new FileInputStream(certS), passS.toCharArray());
+            String alias = ks.aliases().nextElement();
+            
+            X509Certificate cert = (X509Certificate) ks.getCertificate(alias);
+            PrivateKey key = (PrivateKey) ks.getKey(alias, passS.toCharArray());
+            
+            int folio = 1;
+            doc.getDTE().getDocumento().getEncabezado().getIdDoc().setFolio(folio);
+            
+            doc.getDTE().timbrar(caf.getCAF(), caf.getPrivateKey(null));
+            
+            opts.setSavePrettyPrint();
+            opts.setSavePrettyPrintIndent(0);
+            opts.setCharacterEncoding("ISO-8859-1");
+            opts.setSaveImplicitNamespaces(namespaces);
+            
+            // Releo formateado
+            doc = DTEDocument.Factory.parse(doc.newInputStream(opts), opts);
+            
+            // firmo
+            doc.getDTE().sign(key, cert);
+            // Guardo
+            opts = new XmlOptions();
+            opts.setCharacterEncoding("ISO-8859-1");
+            opts.setSaveImplicitNamespaces(namespaces);
+            doc.save(new File("temp.xml"), opts);
+            
+            // Construyo Envio
+            EnvioDTEDocument envio = EnvioDTEDocument.Factory // la plantilla
+                    .parse(plantillaEnvio);
+            
+            // Debo agregar el schema location (Sino SII rechaza)
+            XmlCursor cursor = envio.newCursor();
+            if (cursor.toFirstChild()) {
+                cursor.setAttributeText(new QName(
+                        "http://www.w3.org/2001/XMLSchema-instance",
+                        "schemaLocation"),
+                        "http://www.sii.cl/SiiDte EnvioDTE_v10.xsd");
+            }
+            // leo certificado y llave privada del archivo pkcs12
+            KeyStore ks1 = KeyStore.getInstance("PKCS12");
+            ks1.load(new FileInputStream(certS), passS.toCharArray());
+            String alias1 = ks1.aliases().nextElement();
+            
+            X509Certificate x509 = (X509Certificate) ks1.getCertificate(alias1);
+            String enviadorS = Utilities.getRutFromCertificate(x509);
+            PrivateKey pKey = (PrivateKey) ks1.getKey(alias1, passS.toCharArray());
+            
+            // Asigno un ID
+            envio.getEnvioDTE().getSetDTE().setID(idS);
+            
+            cl.sii.siiDte.EnvioDTEDocument.EnvioDTE.SetDTE.Caratula car = envio
+                    .getEnvioDTE().getSetDTE().getCaratula();
+            
+            car.setRutReceptor(recepS);
+            car.setRutEnvia(enviadorS);
+            
+            // documentos a enviar
+            HashMap<String, String> namespaces1 = new HashMap<String, String>();
+            namespaces1.put("", "http://www.sii.cl/SiiDte");
+            XmlOptions opts1 = new XmlOptions();
+            opts1.setLoadSubstituteNamespaces(namespaces1);
+            
+            
+            DTEDefType[] dtes = new DTEDefType[1];
+            HashMap<Integer, Integer> hashTot = new HashMap<Integer, Integer>();
+            
+            dtes[0] = DTEDocument.Factory.parse(
+                    new FileInputStream("temp.xml"), opts1).getDTE();
+            // armar hash para totalizar por tipoDTE
+            if (hashTot.get(dtes[0].getDocumento().getEncabezado().getIdDoc()
+                    .getTipoDTE().intValue()) != null) {
+                hashTot.put(dtes[0].getDocumento().getEncabezado().getIdDoc()
+                        .getTipoDTE().intValue(), hashTot.get(dtes[0]
+                                .getDocumento().getEncabezado().getIdDoc().getTipoDTE()
+                                .intValue()) + 1);
+            } else {
+                hashTot.put(dtes[0].getDocumento().getEncabezado().getIdDoc()
+                        .getTipoDTE().intValue(), 1);
+            }
+            
+            EnvioDTEDocument.EnvioDTE.SetDTE.Caratula.SubTotDTE[] subtDtes = new EnvioDTEDocument.EnvioDTE.SetDTE.Caratula.SubTotDTE[hashTot.size()];
+            int i = 0;
+            for (Integer tipo : hashTot.keySet()) {
+                EnvioDTEDocument.EnvioDTE.SetDTE.Caratula.SubTotDTE subt = EnvioDTEDocument.EnvioDTE.SetDTE.Caratula.SubTotDTE.Factory.newInstance();
+                subt.setTpoDTE(new BigInteger(tipo.toString()));
+                subt.setNroDTE(new BigInteger(hashTot.get(tipo).toString()));
+                subtDtes[i] = subt;
+                i++;
+            }
+            
+            car.setSubTotDTEArray(subtDtes);
+            
+            opts1 = new XmlOptions();
+            opts1.setSavePrettyPrint();
+            opts1.setSavePrettyPrintIndent(0);
+            envio = EnvioDTEDocument.Factory.parse(envio.newInputStream(opts1));
+            
+            envio.getEnvioDTE().getSetDTE().setDTEArray(dtes);
+            
+            // firmo
+            envio.sign(pKey, x509);
+            opts1 = new XmlOptions();
+            opts1.setCharacterEncoding("ISO-8859-1");
+            String resultS = BASIC_FILES_COMPANIES_PATH+"/DTEs/"+dteType+'/'+folio+".xml";// esto debe ser cambiado por el numero de folio
+            envio.save(new File(resultS), opts1);
+            Logger.getLogger(Iwi_fe.class.getName()).log(Level.FINE, null, "Documento Creado");
+            return true;
+        } catch (XmlException | IOException | KeyStoreException | NoSuchAlgorithmException | CertificateException | UnrecoverableKeyException | NoSuchPaddingException | InvalidKeySpecException | InvalidAlgorithmParameterException | TimbreException | SignatureException | KeyException | MarshalException | XMLSignatureException | SAXException | ParserConfigurationException ex) {
+            Logger.getLogger(Iwi_fe.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return false;
+    }
+}
